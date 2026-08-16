6b56d92343e9d0c987c6dd4cbf4675fde974bfb7
SkyGypsophila <shuangjie.xia01@estudiant.upf.edu>
2026-08-16 21:03:38 +0200

    [ADD] namenode system configuration(settings.json) and /datanodes endponit to retrieve the information

--- projects/1-hdfs/namenode/app/main.py
+++ projects/1-hdfs/namenode/app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from pathlib import Path
+import json
 
 app = FastAPI()
+
+settings_path = Path(__file__).parent.parent / "settings.json"
 
 @app.get("/healthcheck")
 def healthcheck():
     return {"status": "up"}
+
+@app.get("/datanodes")
+def get_datanodes():
+    with open(settings_path) as json_file:
+        settings = json.load(json_file)
+        return {"datanodes": settings["datanodes"]}
 
